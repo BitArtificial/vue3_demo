f38affa7eb3fbeb7739a4d20248c505355528e6b
BitArtificial <45003472+BitArtificial@users.noreply.github.com>
2023-06-27 08:50:59 +0800

    Create Login.vue

---
+++ Login.vue
@@ -0,0 +1,8 @@
+<template>
+    <LoginRegisterForm :isRegister="false"></LoginRegisterForm>
+</template>
+<script setup>
+    import LoginRegisterForm from "./LoginRegisterForm.vue"
+</script>
+<style scoped>
+</style>
